19728c2d5f0ae4a88cc58a27edcccae0853edd18
Robby Ackerley <rcackerley@me.com>
2018-03-28 13:38:58 -0400

    added function to add article to the db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 let db = require('./db.js');
+let getNews = require('./newsAPI');
 
 
 //functions to talk to DB
@@ -11,4 +12,9 @@ let createUser = (username, password, leaning, email) => {
 
 let rateArticle = (userid, articleid, rating) => {
   return db.query(`INSERT INTO ratings(userid, articleid, ${rating}) VALUES('${userid}', '${articleid}', 1);`);
+};
+
+let addArticle = (url, author, description, publishedAt, source, urlToImage, topic) => {
+  return db.query(`INSERT INTO articles(topic, url, author, description, publishedAt, source, urlToImage)
+  VALUES('${topic}', '${url}', '${author}', '${description}', '${publishedAt}', '${source}', '${urlToImage}');`);
 };
