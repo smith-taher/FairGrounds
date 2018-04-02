4ab163433f46bfc7979c03aaa0680fb9c481f512
Robby Ackerley <rcackerley@me.com>
2018-04-02 13:10:19 -0400

    added front end for create account

--- public/scripts/AuthController.js
+++ public/scripts/AuthController.js
@@ -1,4 +1,5 @@
 const loginButton = document.getElementById('login');
+const notUserButton = document.getElementById('creation');
 
 let login = (username, password) => {
   var credentials = {
@@ -7,6 +8,66 @@ let login = (username, password) => {
   };
   return fetch('http://localhost:3000/signin', {method: 'POST', body: JSON.stringify(credentials)})
 };
+
+let generateUserInput = () => {
+  let userInput = document.createElement('input');
+  userInput.setAttribute('placeholder', 'username');
+  return userInput
+}
+
+let generatePassInput = () => {
+  let passInput = document.createElement('input');
+  passInput.setAttribute('placeholder', 'password');
+  return passInput
+}
+
+let generateSlider = () => {
+  var leaningSlider = document.createElement('input');
+  leaningSlider.setAttribute('type', 'range');
+  leaningSlider.setAttribute('min', '1');
+  leaningSlider.setAttribute('max', '100');
+  leaningSlider.setAttribute('value', '50');
+  leaningSlider.setAttribute('class', 'slider');
+  return leaningSlider;
+}
+
+let generateEmailInput = () => {
+  var emailInput = document.createElement('input');
+  emailInput.setAttribute('placeholder', 'email address');
+  return emailInput;
+}
+
+let generateCreateAccountButton = () => {
+  var createButton = document.createElement('button');
+  createButton.textContent = 'Create Account';
+  return createButton;
+}
+
+let removeLoginForm = () => {
+  let username = document.querySelector('#sign-in-page > div > div > input:nth-child(2)')
+  let password = document.querySelector('#sign-in-page > div > div > input:nth-child(3)')
+  username.remove();
+  password.remove();
+  loginButton.remove();
+  notUserButton.remove();
+}
+
+let generateCreateUserForm = () => {
+  let userInput = generateUserInput();
+  let passInput = generatePassInput();
+  let emailInput = generateEmailInput();
+  let leaningSlider = generateSlider();
+  let createButton = generateCreateAccountButton();
+  let signInDiv = document.querySelector('.sign-in-buttons');
+  removeLoginForm();
+  signInDiv.appendChild(userInput);
+  signInDiv.appendChild(passInput);
+  signInDiv.appendChild(emailInput);
+  signInDiv.appendChild(leaningSlider);
+  signInDiv.appendChild(createButton);
+
+
+}
 
 loginButton.addEventListener('click', (event) => {
   let username = document.querySelector('#sign-in-page > div > div > input:nth-child(2)').value;
@@ -19,3 +80,7 @@ loginButton.addEventListener('click', (event) => {
     window.location.href="http://localhost:3000/index.html"
   });
 });
+
+notUserButton.addEventListener('click', (event) => {
+  generateCreateUserForm();
+})
