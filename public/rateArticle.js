c6d543bfbf486a57bc1d3f27c50b2d2d89d5e13f
Joel Smith <joelsmith@Joels-MacBook-Pro.local>
2018-04-04 13:43:39 -0400

    Changed Rate Article buttons with effects

---
+++ public/rateArticle.js
@@ -0,0 +1,27 @@
+// const $fairArticleButton = $(".fair-article-button");
+// const $unfairArticleButton = $(".unfair-article-button");
+
+// $fairArticleButton.click(() => {
+//     console.log("click");
+//     signIn.validateCredentials();
+//     console.log(user.id);
+//     $.post("http://localhost:3000/ratings", {
+
+//     });
+//     getDBArticleForRating();
+//     console.log(articles.id);
+//     postRating(articles.id, users.id);
+// })
+
+// $unfairArticleButton.click(() => {
+//     $postRating();
+// })
+
+
+//Psuedo Code
+// When rate Fair/Unfair
+// Pull Article ID and User ID
+// Select user ID and article ID from ratings
+//  [], [rating]
+//  if empty insert
+//  if not respond with already rated
