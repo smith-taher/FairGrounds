c94c48cdc20ebf7fac1d1967f7c7824f945e85cc
rfpoulos <rfpoulos@outlook.com>
2018-04-09 17:45:23 -0400

    debugging

--- public/frontend.js
+++ public/frontend.js
@@ -229,16 +229,13 @@ let getDBArticleForRating = (articleToGet, userid) => {
         {method: 'POST', body: JSON.stringify(idObject)})
         .then(response => response.json())
             .then((articles) => {
-                if (articles.length === 0) {
-                    let $noMore = $('<div></div>').addClass('no-more');
+                    printArticlesForRating(articles, $printArticleForRating, 0);
+            }).catch(data => {
+                let $noMore = $('<div></div>').addClass('no-more');
                     $noMore.text('No more articles to rate.  Check back later!');
                     $printArticleForRating.append($noMore);
-                } else {
-                    printArticlesForRating(articles, $printArticleForRating, 0);
-                }
-            }
-    )
-}
+            })
+        }
 
 let getDBArticlesView  = () => {
     $.get('/articles', data => {
